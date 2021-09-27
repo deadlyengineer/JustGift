c4f08d218ca75b9f97786fa67399b0e6f52119b8
Topaz <bolesalavb@gmail.com>
2021-09-26 20:13:45 -0700

    Add Avatar component

--- screens/components/avatar.js
+++ screens/components/avatar.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import TextAvatar from 'react-native-text-avatar';
 import FastImage from 'expo-fast-image';
 
 const Avatar = (props) => {
     
+    if(props.source == null)
+        return (
+            <TextAvatar
+                backgroundColor={'gray'}
+                textColor={'white'}
+                size={props.size}
+                type={'circle'}
+            >{props.firstName + ' ' + props.lastName}</TextAvatar>
+        );
+
     return (
-        <></>
+        <FastImage source={{ uri: props.source }} style={{ width: props.size, height: props.size, borderRadius: props.size / 2 }}/>
     );
 }
 
